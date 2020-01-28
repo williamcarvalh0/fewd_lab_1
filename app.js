bfdee826dab426f8304c3f3231b68e84dcff031f
williamcarvalh0 <williamcarvalh0@hotmail.com>
2020-01-28 15:30:36 +0000

    commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var firstName = 'William';
+var favouriteColor = 'Green';
+var favouriteFood = 'Barbecue';
+var city = 'Dublin';
+
+// the console.log() will print at the terminal all the variables declared.
+console.log(firstName, favouriteColor, favouriteFood, city);
+
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'Orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+console.log(phrase+one+", "+two+", "+three+", "+four+", "+five+", "+six+", "+seven+", "+endingPhrase);
